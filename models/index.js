6eff839d1f74115869b29bbdcf7a09379d219beb
Stanislav Kharitonov <q3introx@mail.ru>
2018-08-04 18:30:23 +0400

    debug heroku

--- models/index.js
+++ models/index.js
@@ -6,7 +6,8 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
-
+console.log('ENV', env);
+console.log(config);
 const sequelize = config.use_env_variable
   ? new Sequelize(process.env[config.use_env_variable], config)
   : new Sequelize(config.database, config.username, config.password, config);
